74ea0ecef4499ff9dcf680c3db37297d8883831b
Oarabile Mbewe <oarabilembewe@DVTLUNLVCH.local>
2026-02-28 15:43:12 +0200

    Application basic functionality

--- TEMPAPP/src/App.tsx
+++ TEMPAPP/src/App.tsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 
+function formulae(Farenheid: number , celcius:number, isConvertedToFarenHeid: boolean){
+  function celciusToFarenheid():number{
+    return (9/5) * celcius + 32;
+  }
+
+  function farenheidToCelcius():number{
+    return (5/9) * (Farenheid - 32);
+  }
+
+  let finalValue = isConvertedToFarenHeid ? celciusToFarenheid() : farenheidToCelcius();
+  console.log(finalValue);
+}
+
 function App() {
   const [value, setValue] = useState(0);
   return(
     <div>
-      <input type="text" value={value}/>
-      <input type="text" />
+      <input type="text" onChange={(e)=>{setValue(Number(e.target.value))}}/>
+      <button onClick={()=>formulae(value,value,true)}>Convert To Farenheid</button>
+      <input type="text" onChange={(e)=>{setValue(Number(e.target.value))}}/>
+      <button onClick={()=>formulae(value,value,false)}>Convert To Celcius</button>
     </div>
   )
 }
